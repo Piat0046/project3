bc9c596766878b4b46c138e203bc78ed8205b3d8
ji hun park <piat@jiui-MacBookAir.local>
2022-02-16 00:32:58 +0900

    please

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -101,100 +101,101 @@ def create_app():
             url_nick = f'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{parse.quote_plus(nick)}'
 
             m = check2(url_nick)
-            #if nick == "":
-            #    m = 'fail'
+            if nick == "":
+                m = '닉네임을 입력하세요'
 
-            if m == 'fail':
-                m = '소환사를 찾을 수 없습니다.'
-                print('소환사를 찾을 수 없습니다.')
-
-            elif m == 'pass':
-                nick_to_id = requests.get(url_nick, headers=request_header).json()
-                input_id = nick_to_id['id']
-
-                url_game = f'https://kr.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/{input_id}'
-                m = check2(url_game)
-                
+            else:
                 if m == 'fail':
-                    m = '소환사가 게임중이 아닙니다'
-                    print('소환사가 게임중이 아닙니다')
+                    m = '소환사를 찾을 수 없습니다.'
+                    print('소환사를 찾을 수 없습니다.')
 
                 elif m == 'pass':
+                    nick_to_id = requests.get(url_nick, headers=request_header).json()
+                    input_id = nick_to_id['id']
 
-                    data = requests.get(url_game, headers=request_header).json()
-
-                    dic = []
-                    for i in range(0,10):
-                        summoner_Id = data['participants'][i]['summonerId']
-                        dic.append(summoner_Id)
-                    print(dic)
-
-                    dic = []
-
-                    for i in range(0,10):
-                        champ_id = data['participants'][i]['championId']
-                        summoner_Id = data['participants'][i]['summonerId']
-                        champ_status_url = f"https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{summoner_Id}/by-champion/{champ_id}"
-                        m = check2(champ_status_url)
-                        print(m)
-                        champ_status = requests.get(champ_status_url, headers=request_header).json()
-                        champ_point = champ_status['championPoints']
-                        dic.append(champ_id)
-                        dic.append(champ_point)
-
-                    import pickle
-                    import sqlite3
-                    import pandas as pd
-                    from lightgbm import LGBMClassifier, plot_importance
-                    from xgboost import XGBClassifier
-
-                    #def create_app():
-
-                    app = Flask(__name__)
-
-                    model = None
-
-                    with open('model4.pkl', 'rb') as file:
-                        model = pickle.load(file)
-
-                    columns = ['1', '1score', '2', '2score', '3', '3score', '4', '4score', '5', '5score', '6', '6score', '7', '7score', '8', '8score', '9', '9score', '10', '10score']
-                    df = pd.DataFrame(columns = columns)
-                    input_data = {
-                                '1' : dic[0],
-                                '1score' : dic[1],
-                                '2' : dic[2],
-                                '2score' :dic[3],
-                                '3' : dic[4],
-                                '3score' : dic[5],
-                                '4' : dic[6],
-                                '4score' : dic[7],
-                                '5' : dic[8],
-                                '5score' : dic[9],
-                                '6' : dic[10],
-                                '6score' : dic[11],
-                                '7' : dic[12],
-                                '7score' : dic[13],
-                                '8' : dic[14],
-                                '8score' : dic[15],
-                                '9' : dic[16],
-                                '9score' : dic[17],
-                                '10' : dic[18],
-                                '10score' : dic[19],
-                                }
-                    X_test = df.append(input_data, ignore_index=True)
-                    X_test = X_test.astype(int)
-                    y_pred = list(model.predict(X_test))[0]
-                    c=model.predict_proba(X_test).reshape(-1,1)
+                    url_game = f'https://kr.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/{input_id}'
+                    m = check2(url_game)
                     
-                    blue_team = int(list(c[0])[0]*100)
-                    red_team = int(list(c[1])[0]*100)
-                    
-                    if y_pred == 200:
-                        y_pred = '레드팀'
-                    else:
-                        y_pred = '블루팀'
+                    if m == 'fail':
+                        m = '소환사가 게임중이 아닙니다'
+                        print('소환사가 게임중이 아닙니다')
 
-                    m = f'예상승리팀={y_pred}, 예상 확률 블루팀={int(list(c[0])[0]*100)}%, 레드팀{int(list(c[1])[0]*100)}% '
+                    elif m == 'pass':
+
+                        data = requests.get(url_game, headers=request_header).json()
+
+                        dic = []
+                        for i in range(0,10):
+                            summoner_Id = data['participants'][i]['summonerId']
+                            dic.append(summoner_Id)
+                        print(dic)
+
+                        dic = []
+
+                        for i in range(0,10):
+                            champ_id = data['participants'][i]['championId']
+                            summoner_Id = data['participants'][i]['summonerId']
+                            champ_status_url = f"https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{summoner_Id}/by-champion/{champ_id}"
+                            m = check2(champ_status_url)
+                            print(m)
+                            champ_status = requests.get(champ_status_url, headers=request_header).json()
+                            champ_point = champ_status['championPoints']
+                            dic.append(champ_id)
+                            dic.append(champ_point)
+
+                        import pickle
+                        import sqlite3
+                        import pandas as pd
+                        from lightgbm import LGBMClassifier, plot_importance
+                        from xgboost import XGBClassifier
+
+                        #def create_app():
+
+                        app = Flask(__name__)
+
+                        model = None
+
+                        with open('model4.pkl', 'rb') as file:
+                            model = pickle.load(file)
+
+                        columns = ['1', '1score', '2', '2score', '3', '3score', '4', '4score', '5', '5score', '6', '6score', '7', '7score', '8', '8score', '9', '9score', '10', '10score']
+                        df = pd.DataFrame(columns = columns)
+                        input_data = {
+                                    '1' : dic[0],
+                                    '1score' : dic[1],
+                                    '2' : dic[2],
+                                    '2score' :dic[3],
+                                    '3' : dic[4],
+                                    '3score' : dic[5],
+                                    '4' : dic[6],
+                                    '4score' : dic[7],
+                                    '5' : dic[8],
+                                    '5score' : dic[9],
+                                    '6' : dic[10],
+                                    '6score' : dic[11],
+                                    '7' : dic[12],
+                                    '7score' : dic[13],
+                                    '8' : dic[14],
+                                    '8score' : dic[15],
+                                    '9' : dic[16],
+                                    '9score' : dic[17],
+                                    '10' : dic[18],
+                                    '10score' : dic[19],
+                                    }
+                        X_test = df.append(input_data, ignore_index=True)
+                        X_test = X_test.astype(int)
+                        y_pred = list(model.predict(X_test))[0]
+                        c=model.predict_proba(X_test).reshape(-1,1)
+                        
+                        blue_team = int(list(c[0])[0]*100)
+                        red_team = int(list(c[1])[0]*100)
+                        
+                        if y_pred == 200:
+                            y_pred = '레드팀'
+                        else:
+                            y_pred = '블루팀'
+
+                        m = f'예상승리팀={y_pred}, 예상 확률 블루팀={int(list(c[0])[0]*100)}%, 레드팀{int(list(c[1])[0]*100)}% '
         
             return render_template('index.html', nick = sel_nick, champ=sel_champ, num = num, username = nick, m = m)
 
